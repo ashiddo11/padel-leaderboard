4065514ebfbecb47c1cabd787a4b86038e7e8229
abdelrahman shiddo <abdelrahman.shiddo@medable.com>
2024-12-10 02:02:12 +0400

    postgres

--- app/models/index.js
+++ app/models/index.js
@@ -4,9 +4,15 @@ const basename = path.basename(__filename);
 const db = {};
 import {PostgresDialect} from '@sequelize/postgres';
 
-const sequelize = new Sequelize('neondb', 'neondb_owner', 'Gk9UKJvOPcx0', {
-  host: 'ep-weathered-rice-a2sx4zuf-pooler.eu-central-1.aws.neon.tech',
-  dialect: PostgresDialect,
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 db.sequelize = sequelize;
